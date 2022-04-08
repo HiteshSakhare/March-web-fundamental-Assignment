2fb6d5920c062526b5bc50975b71658cecd83056
Hitesh Sakhare <99487579+HiteshSakhare@users.noreply.github.com>
2022-04-08 18:08:12 +0530

    Add files via upload

---
+++ Assignment 13/Task3/Task3.js
@@ -0,0 +1,11 @@
+let num1=prompt("Enter Any Number")
+
+let numA1=parseInt(num1)
+
+if(numA1 % 2==0){
+    console.log(numA1, "Given number is even number.")
+}
+else{
+    console.log( numA1, "Given number is even number.")
+}
+
