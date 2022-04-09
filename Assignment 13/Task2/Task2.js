bc83632b46bc8ee28ab0de3f277dc2a7d8319016
Hitesh Sakhare <99487579+HiteshSakhare@users.noreply.github.com>
2022-04-09 17:20:17 +0530

    Delete Assignment 13 directory

--- Assignment 13/Task2/Task2.js
+++
@@ -1,8 +0,0 @@
-let str=prompt("Enter any Charecter.")
-
-if(str=='A' || str=='E' || str=='I' || str=='O' || str=='U' || str=='a' || str=='e' || str=='i' || str=='o' || str=='u'){
-    console.log("Given charecter is vovel.")
-}
-else{
-    console.log("Given charecter is Not vovel.")
-}
